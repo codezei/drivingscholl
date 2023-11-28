9b0ca3bea0facc2c33d6797b2bb7a598d0d28726
codezei <mwbox89@gmail.com>
2023-11-28 21:17:25 +0200

    some fix

--- app/js/app.js
+++ app/js/app.js
@@ -15,7 +15,23 @@ document.addEventListener('DOMContentLoaded', () => {
 		mobile: true,
 		live: true}).init()
 	smoothScroll() 
+	lazyVideo()
+	parkSlider()
 })
+
+if (document.documentElement.clientWidth < 480) {
+	window.addEventListener('scroll',
+	  function () {
+		return setTimeout(lazyMap, 1000)
+	  }, {
+		once: true,
+		passive: true
+	  });
+  } else {
+	lazyMap();
+  };
+
+
 function smoothScroll() {
 	let linkNav = document.querySelectorAll('[href^="#"]')
 	let headerHeight = document.querySelector('.header').getBoundingClientRect().height
@@ -136,4 +152,61 @@ function gallerySlider() {
 			},
 		}
 	});
+}
+function parkSlider() {
+	let swiper = new Swiper(".park-swiper", {
+		slidesPerView: 4,
+		spaceBetween: 24,
+		// loop: true,
+		navigation: {
+			nextEl: ".park-button-next",
+			prevEl: ".park-button-prev",
+		},
+		pagination: {
+			el: ".park-pagination",
+			clickable: true
+		  },
+
+		breakpoints: {
+			1200: {
+				slidesPerView: 4,
+			},
+			992: {
+				slidesPerView: 3,
+			},
+			0: {
+				slidesPerView: 2
+			},
+		}
+	});
+}
+
+function lazyVideo () {
+	let videos = document.querySelectorAll('.lazy-video')
+	for (let i = 0; i < videos.length; i++) {
+		let thumbnail = document.createElement('img')
+		thumbnail.setAttribute('src', 'https://i.ytimg.com/vi/'+videos[i].dataset.id+'/hqdefault.jpg')
+		thumbnail.setAttribute('alt', '')
+		videos[i].appendChild(thumbnail)
+
+		videos[i].addEventListener('click', function (e) {
+			let iframe = document.createElement('iframe')
+			iframe.setAttribute('src', 'https://www.youtube.com/embed/'+e.currentTarget.dataset.id)
+			iframe.classList.add('iframe')
+			iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture')
+			e.currentTarget.replaceWith(iframe)
+		})
+	}
+}
+
+function lazyMap () {
+	let maps = document.querySelectorAll('.lazy-map')
+
+	for (let i = 0; i < maps.length; i++) {
+		let iframe = document.createElement('iframe')
+		iframe.setAttribute('src', maps[i].dataset.src)
+		iframe.setAttribute('height', 450)
+		iframe.classList.add('iframe')
+		maps[i].replaceWith(iframe)
+	}
 }
